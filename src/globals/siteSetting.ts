ecdd1523db903e0e70cad5cbc3b034b15ce6010e
Rajat Thapa <rajatthapa826@gmail.com>
2026-01-15 21:25:17 +1100

    changes to fix on vercel

--- src/globals/siteSetting.ts
+++ src/globals/siteSetting.ts
@@ -1,7 +1,7 @@
 import type { GlobalConfig } from 'payload'
 
-export const siteSetting: GlobalConfig = {
-  slug: 'siteSetting',
+export const SiteSetting: GlobalConfig = {
+  slug: 'site-setting',
   access: {
     read: () => true,
   },
