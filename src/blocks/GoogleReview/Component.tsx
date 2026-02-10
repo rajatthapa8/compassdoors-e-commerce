d18074bf2d2eab674ab82a8736606286d470370e
Rajat Thapa <rajatthapa826@gmail.com>
2026-02-10 22:08:52 +1100

    hero and google section UI fix

--- src/blocks/GoogleReview/Component.tsx
+++ src/blocks/GoogleReview/Component.tsx
@@ -34,43 +34,45 @@ export default async function GoogleReviewBlock({ title, limit }: GoogleReviewBl
   if (!reviews.length) return null
 
   return (
-    <section className="h-screen container flex flex-col justify-center items-left">
-      <div className="flex justify-between items-center">
-        <h2 className="text-3xl md:text-4xl font-bold text-[#2f2f2f] mb-16">{title}</h2>
-        <Link href="https://maps.app.goo.gl/ZFkFYcwoMQTLMqrZ6" target="_blank">
-          <h2 className="text-md md:text-md underline font-bold text-[#2f2f2f] mb-16">
-            View Google Map
-          </h2>
-        </Link>
-      </div>
-
-      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 max-w-6xl mx-auto">
-        {displayedReviews.map((review, i) => (
-          <Link
-            href={review.author_url}
-            key={i}
-            target="_blank"
-            className="w-full max-w-88 space-y-4 rounded-md border border-gray-200 bg-white p-3 text-gray-500 transition-all duration-300 hover:-translate-y-1"
-          >
-            <div className="flex items-center justify-between">
-              <div className="flex flex-row">
-                <StarRating rating={review.rating} />
-              </div>
-              <p>{review.relative_time_description}</p>
-            </div>
-            <p>{review.text}</p>
-            <div className="flex items-center gap-2 pt-3">
-              <Image
-                className="h-8 w-8 rounded-full"
-                src={review.profile_photo_url}
-                alt="Richard Nelson"
-                height={500}
-                width={500}
-              />
-              <p className="font-medium text-gray-800">{review.author_name}</p>
-            </div>
+    <section className=" bg-white">
+      <div className="h-screen container flex flex-col justify-center items-left">
+        <div className="flex justify-between items-center">
+          <h2 className="text-3xl md:text-4xl font-bold text-[#2f2f2f] mb-16">{title}</h2>
+          <Link href="https://maps.app.goo.gl/ZFkFYcwoMQTLMqrZ6" target="_blank">
+            <h2 className="text-md md:text-md underline font-bold text-[#2f2f2f] mb-16">
+              View Google Map
+            </h2>
           </Link>
-        ))}
+        </div>
+
+        <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 max-w-6xl mx-auto">
+          {displayedReviews.map((review, i) => (
+            <Link
+              href={review.author_url}
+              key={i}
+              target="_blank"
+              className="w-full max-w-88 space-y-4 rounded-md border border-gray-200 bg-white p-3 text-gray-500 transition-all duration-300 hover:-translate-y-1"
+            >
+              <div className="flex items-center justify-between">
+                <div className="flex flex-row">
+                  <StarRating rating={review.rating} />
+                </div>
+                <p>{review.relative_time_description}</p>
+              </div>
+              <p>{review.text}</p>
+              <div className="flex items-center gap-2 pt-3">
+                <Image
+                  className="h-8 w-8 rounded-full"
+                  src={review.profile_photo_url}
+                  alt="Richard Nelson"
+                  height={500}
+                  width={500}
+                />
+                <p className="font-medium text-gray-800">{review.author_name}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </section>
   )
--- src/heros/HighImpact/index.tsx
+++ src/heros/HighImpact/index.tsx
@@ -15,7 +15,7 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText 
   })
 
   return (
-    <div className="-mt-[10.4rem] flex items-center justify-center text-white" data-theme="dark">
+    <div className="flex items-center justify-center text-white" data-theme="dark">
       <div className="container mb-8 z-10 relative flex items-center justify-items-start">
         <div className="max-w-[36.5rem] md:text-center">
           {richText && <RichText className="mb-6 text-left" data={richText} enableGutter={false} />}
