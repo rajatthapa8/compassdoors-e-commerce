c921c492355cfa3ebb37c5b621d11542b8ddeee7
Rajat Thapa <rajatthapa826@gmail.com>
2026-07-19 14:24:03 +1000

    config changed to be suited as per cloudfare instead of aws

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -102,12 +102,14 @@ export default buildConfig({
         media: true,
       },
       bucket: process.env.S3_BUCKET || '',
+
       config: {
         credentials: {
           accessKeyId: process.env.S3_ACCESS_KEY_ID || '',
           secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || '',
         },
         region: process.env.S3_REGION,
+        endpoint: process.env.S3_ENDPOINT,
         forcePathStyle: false,
       },
     }),
